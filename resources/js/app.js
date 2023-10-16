a29f54dbd8153e35bfca3a49750458b8f7fa3eab
tanto <tantows001@gmail.com>
2023-10-16 23:48:18 +0700

    feat: register

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,1 @@
 import './bootstrap';
-import 'flowbite';
-
-document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll("#navbar");
-
-    links.forEach((link) => {
-        if (link.getAttribute("href") === window.location.pathname) {
-            link.classList.add("active");
-            link.classList.remove("hover:bg-gray-100")
-        }
-    });
-});
